648e071df16c859cbb596f0fe46d1c46557351a4
jagangitrepo <sb.jagankumar@gmail.com>
2019-07-28 02:20:11 +0530

    1 file modified

--- src/app/grid.component.ts
+++ src/app/grid.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy , ChangeDetectorRef} from '@angular/core';
 
 @Component({
   selector: 'grid-elem',
-  template: `<button style="width:100px; height: 100px;" [style.background]="bgcolor" (click)="toggle()">{{index}}</button> {{check()}}`,
+  template: `<button style="width:100px; height: 100px;" [style.background]="bgcolor" (click)="toggle()">{{index}}</button> <p>{{ check() }}</p>`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GridElementComponent  {
   @Input() index: string;
   @Input() bgcolor: string;
+  constructor(private cd:ChangeDetectorRef){}
   toggle(){
     ((this.bgcolor === "red")? this.bgcolor = "" : this.bgcolor = "red");
+    //this.cd.detectChanges(); // Start with triggerred component and also trigger from the root.
+    //this.cd.markForCheck(); // Trigger from the root view node on next Cd cycle.
   }
   check()
   {
@@ -19,11 +22,12 @@ export class GridElementComponent  {
 
 @Component({
   selector: 'grid-list',
-  template: `<div class="wrapper"> <grid-elem  *ngFor="let obj of ilist" [index]="obj" [bgcolor]="blue"> {{check()}}</grid-elem> </div>`,
+  template: `<div class="wrapper">  <grid-elem  *ngFor="let obj of ilist" [index]="obj" [bgcolor]="blue"> </grid-elem></div> <p>{{ check() }}</p>`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GridListComponent  {
   @Input() ilist: string[];
+
   check()
   {
       console.log("Grid-List: Checked..")
@@ -32,10 +36,10 @@ export class GridListComponent  {
 
 @Component({
   selector: 'grid',
-  template: `<grid-list [ilist]="list"> {{check()}} </grid-list>`
+  template: `<grid-list [ilist]="list"></grid-list> <p>{{ check() }}</p> `
 })
 export class GridComponent  {
-  list=["1","2","3","1","2","3", "1","2","3","1","2","3"  ];
+  list=["1","2","3","4","5","6","7","8","9"];
   check()
   {
       console.log("Grid-Component: Checked..")
